b47b04011927c355b3be3904e1780dd019f19af1
SergoRz <sanchez13rz@gmail.com>
2017-02-01 23:19:09 +0100

    yee!

--- app/src/main/java/com/example/versus/cualesmi_ip/MainActivity.java
+++ app/src/main/java/com/example/versus/cualesmi_ip/MainActivity.java
@@ -108,7 +108,7 @@ public class MainActivity extends Activity {
                 }
 
                 Log.d("Linea: ", linea);
-                Pattern patron = Pattern.compile("(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])");
+                Pattern patron = Pattern.compile("([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])");
                 //Creamos el Matcher a partir del patron, la cadena como parametro
                 Matcher matcher = patron.matcher(linea);
 
